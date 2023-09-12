cc54f2277578154ac9336214b851c2b5b102fd4e
seregatotsamyi1~ <iserg.mos.ls@gmail.com>
2023-09-12 10:07:31 +0500

    popup

--- app/js/main.js
+++ app/js/main.js
@@ -181,4 +181,27 @@ $(function () {
   });
 
   //click outside
+
+
+  //btn-hover
+  const btns = document.querySelectorAll('.button')
+
+  btns.forEach(el => {
+    el.addEventListener('mouseover', function (e) {
+      const
+        size = Math.max(this.offsetWidth, this.offsetHeight),
+        x = e.offsetX - size / 2,
+        y = e.offsetY - size / 2,
+        wave = document.createElement('span')
+
+      // Create a new wave
+      wave.className = 'wave'
+      wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`
+      this.appendChild(wave)
+
+      // Remove element after animation ends
+      setTimeout(() => wave.remove(), 500)
+    })
+  })
+  //btn-hover
 });
